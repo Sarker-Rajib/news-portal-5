43a5444e1962ef59e979bdc3aad3edbb835fdef5
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-09-03 16:17:49 +0600

    news number counter added

--- js/app.js
+++ js/app.js
@@ -1,47 +1,60 @@
 // category nav 
 const loadCategory = () => {
-   const url = `https://openapi.programming-hero.com/api/news/categories`;
-   fetch(url)
-       .then(res => res.json())
-       .then(data => showCategory(data.data.news_category))
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showCategory(data.data.news_category))
 }
 
 const showCategory = (newsCategory) => {
-   const categoriesWrapper = document.getElementById('content-container');
+    const categoriesWrapper = document.getElementById('content-container');
 
-   console.log(newsCategory);
-   newsCategory.forEach(category => {
-       const categoryTab = document.createElement('div');
-       categoryTab.classList.add('col', 'text-center');
-       categoryTab.innerHTML = `
+    console.log(newsCategory);
+    newsCategory.forEach(category => {
+        const categoryTab = document.createElement('div');
+        categoryTab.classList.add('col', 'text-center');
+        categoryTab.innerHTML = `
            <p class="border rounded px-2 mb-0 text-nowrap category-nav" onclick="loadNewaContent(${category.category_id})">${category.category_name}</p>
        `
-       categoriesWrapper.appendChild(categoryTab);
-   });
+        categoriesWrapper.appendChild(categoryTab);
+    });
 }
 
 
 const loadNewaContent = (id = 8) => {
-   const url = `https://openapi.programming-hero.com/api/news/category/0${id}`
-   fetch(url)
-       .then(res => res.json())
-       .then(data => showNewsContent(data.data))
+    const url = `https://openapi.programming-hero.com/api/news/category/0${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showNewsContent(data.data))
 };
 
 const showNewsContent = (contents) => {
-   const newsContainer = document.getElementById('news-wrspper');
-   newsContainer.innerHTML = '';
+    const newsContainer = document.getElementById('news-wrspper');
+    newsContainer.innerHTML = '';
 
-   contents.forEach(content => {
-       console.log(content);
-       const card = document.createElement('div');
-       card.classList.add('card','mb-3');
-       card.innerHTML = `
+    const newsCount = contents.length;
+    const newsCountContainer = document.getElementById('count-message-container');
+    if (newsCount === 0) {
+        newsCountContainer.innerHTML = `
+       <h4>${newsCount} / No News Found</h4>
+       `
+    }
+    else {
+        newsCountContainer.innerHTML = `
+       <h4>${newsCount} News Found</h4>
+       `
+    }
+
+    contents.forEach(content => {
+        console.log(content);
+        const card = document.createElement('div');
+        card.classList.add('card', 'mb-3');
+        card.innerHTML = `
            <div class="row g-0">
-               <div class="col-md-5 mb-2">
-                   <img src="${content.thumbnail_url}" class="img-fluid rounded-start" alt="image">
+               <div class="col-md-4">
+                   <img src="${content.thumbnail_url}" class="w-100 img-fluid rounded-start" alt="image">
                </div>
-               <div class="col-md-7">
+               <div class="col-md-8">
                    <div class="card-body">
                        <h5 class="card-title">${content.title}</h5>
                        <p class="card-text">${content.details.slice(0, 300)}...</p>
@@ -50,8 +63,8 @@ const showNewsContent = (contents) => {
                </div>
            </div>
        `
-       newsContainer.appendChild(card);
-   })
+        newsContainer.appendChild(card);
+    })
 }
 
 loadNewaContent();
